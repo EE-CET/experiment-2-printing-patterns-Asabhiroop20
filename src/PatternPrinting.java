7ea603758721a24037aec68b3a4987f6ab921e8e
Asabhiroop20 <abhiroopas2423@gmail.com>
2026-03-17 22:25:55 +0530

    Second Update Patternprint

--- src/PatternPrinting.java
+++ src/PatternPrinting.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class PatternPrinting {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter Number of Stars");
         int n= sc.nextInt();
         for (int i=0;i< n;i++){
             System.out.print("* ");
